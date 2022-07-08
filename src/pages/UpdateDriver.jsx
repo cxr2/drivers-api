deda2c61a32b4c34fc11419257be78c49547e267
cxr2 <ciosahoulihan@gmail.com>
2022-07-08 19:05:35 +0100

    added update function

--- src/pages/UpdateDriver.jsx
+++ src/pages/UpdateDriver.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import DriverForm from "./../components/forms/DriverForm";
+
+import { DriversContext } from "./../contexts/driver.context";
 
 export default function UpdateDriver() {
-  return <div>UpdateDriver</div>;
+  const { drivers, updateDriver } = useContext(DriversContext);
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  //find id and pass it down
+  const driver = drivers.find(({ _id }) => _id === id);
+
+  const handler = (id, data) => {
+    updateDriver(id, data);
+    //update car and navigate back to homepage
+    navigate("/");
+  };
+
+  return (
+    <>
+      <h1>Update Driver</h1>
+      <DriverForm submitHandler={handler} driver={driver} />
+    </>
+  );
 }
